070d526cf23114f7a4b90e6005572f9d16b91607
dimaluz <dimaluz@rambler.ru>
2024-04-09 16:34:47 +0100

    first commit

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,15 @@
+
+import { RegisterForm } from '@/components/auth/register-form'
+import React from 'react'
+
+type Props = {}
+
+const RegisterPage = (props: Props) => {
+  return (
+    <div>
+        <RegisterForm />
+    </div>
+  )
+}
+
+export default RegisterPage
