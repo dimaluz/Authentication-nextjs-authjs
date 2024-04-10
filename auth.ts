3511fd4188beb720132802e4c2d89630c88c546e
dimaluz <dimaluz@rambler.ru>
2024-04-10 10:28:31 +0100

    Google and Github providers were added

--- auth.ts
+++ auth.ts
@@ -1,7 +1,28 @@
 import NextAuth from "next-auth"
+import { JWT } from "next-auth/jwt"
+
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { db } from "./lib/db"
 import authConfig from "@/auth.config"
+
+
+import { getUserById } from "./data/user"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string,
+      role: "ADMIN" | "USER"
+    }
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    role?: "ADMIN" | "USER"
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -9,15 +30,53 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  pages:{
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+
+  events: {
+    async linkAccount({user}){
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date()}
+      })
+    }
+  },
   callbacks: {
+    // async signIn({ user }) {
+
+    //   const existedUser = await getUserById(user.id);
+
+    //   if (!existedUser || !existedUser.emailVarified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+
     async session({ token, session }){
-      if(token.sub && session.user){
+      
+      if (token.sub && session.user) {
         session.user.id = token.sub
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
       return session;
     },
+
     async jwt({ token }){
-      console.log({token})
+      
+      if(!token.sub) return token;
+
+      const existedUser = await getUserById(token.sub);
+
+      if (!existedUser) return token;
+
+      token.role = existedUser.role;
       return token;
     }
   },
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -4,6 +4,7 @@ import * as z from 'zod'
 import { CardWrapper } from "./card-wrapper"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useSearchParams } from 'next/navigation'
 
 import { LoginSchema } from '@/schemas'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
@@ -15,6 +16,11 @@ import { login } from '@/actions/login'
 import { useState, useTransition } from 'react'
 
 export const LoginForm = () => {
+
+    const searchParams = useSearchParams()
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+        ? "Email already in use with different provider!"
+        : "";
 
     const [error, setError] = useState<string | undefined>('')
     const [success, setSuccess] = useState<string | undefined>('')
@@ -34,8 +40,8 @@ export const LoginForm = () => {
         startTransition(() => {
             login(values)
              .then((data) => {
-                setError(data.error);
-                setSuccess(data.success);
+                setError(data?.error);
+                // setSuccess(data?.success);
              })
         })
     }
@@ -94,7 +100,7 @@ export const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
                     <Button
                         type='submit'
